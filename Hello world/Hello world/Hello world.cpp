60659c66a8f8dabeae0765def6ef9183dda7c80d
XxdalendxX <connordr112@gmail.com>
2022-05-19 11:58:00 +1000

    Revert "Revert "Added introduction feature into project""
    This reverts commit fb89f1115dae96075c006188e7b972ff0c645fc5.

--- Hello world/Hello world/Hello world.cpp
+++ Hello world/Hello world/Hello world.cpp
@@ -5,7 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int age;
+    std::cin >> age;
+    
+    std::cout << "Hello World! I am " << age << " years old!";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
